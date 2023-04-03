d4e7491eb1b0196e0414adf1ba05b22bdd532870
DOHYEON KIM <dpotlight@naver.com>
2023-04-03 09:37:01 +0900

    Add files via upload

---
+++ candy.py
@@ -0,0 +1,17 @@
+#현재 5000원이 있고 사탕의 가격이 120원이라고 하자.
+#최대한 살 수 있는 사탕의 개수와 나머지 돈은 얼마인가?
+
+budget = 5000
+candy = 120
+
+cnt = budget // candy
+change = budget % candy
+
+print(budget,'원으로 살 수 있는 사탕의 개수는', candy,'개이고 잔돈', change,'원입니다')
+
+
+
+
+
+
+
---
+++ turtle.py
@@ -0,0 +1,28 @@
+# . --> ~에서 가져온다
+# t= turtle.Turtle() ---> turtle모듈의 Turtle()메서드를 이용해 거북이 객체 t를 생성
+# 메서드(Method)함수란? _
+# 모듈, 패키지, 함수(메서드)
+
+
+t.forward(50)
+t.fd(50)
+t.left(90)
+t.shape("turtle")
+t.fd(100)
+t.lt(90)
+t.circle(50)
+t.rt(90)
+
+
+t1 = turtle.Turtle("turtle")
+t1. fd(200)
+t1. color("red")
+t1.rt(90)
+t1.circle(50)
+t1.begin_fill()
+t1.circle(50)
+t1.fillcolor("red")
+t1.circle(50)
+
+t1.rt(90)
+t1.circle(50)
